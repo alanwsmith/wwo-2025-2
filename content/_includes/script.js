e63540f9899e65a997626139b67a3d6e707adea7
Alan Smith <commit@alanwsmith.com>
2025-10-19 12:09:06 -0400

    finished moving to latest tempalte

--- content/_includes/script.js
+++ content/_includes/script.js
@@ -114,8 +114,6 @@ function updateClosest(stateName) {
       }
     }
   });
-  console.log(s.neighbors);
-  console.log(s.distance);
 }
 
 function missingLetters(a, b) {
@@ -173,25 +171,17 @@ The states with the most matching letters are:
 
   makeMap() {
     const us = s.data;
-
     const width = 975;
     const height = 610;
-
-    // const zoom = d3.zoom()
-    //   .scaleExtent([1, 8])
-    //   .on("zoom", zoomed);
 
     const svg = d3.create("svg")
       .attr("viewBox", [0, 0, width, height])
       .attr("width", width)
       .attr("height", height)
       .attr("style", "max-width: 100%; height: auto;");
-    //  .on("click", reset);
 
     const path = d3.geoPath();
-
     const g = svg.append("g");
-
     const states = g.append("g")
       .attr("fill", "#444")
       .attr("cursor", "pointer")
@@ -210,51 +200,13 @@ The states with the most matching letters are:
       .attr("stroke-linejoin", "round")
       .attr("d", path(topojson.mesh(us, us.objects.states, (a, b) => a !== b)));
 
-    // svg.call(zoom);
-
-    // function reset() {
-    //   states.transition().style("fill", null);
-    //   svg.transition().duration(750).call(
-    //     zoom.transform,
-    //     d3.zoomIdentity,
-    //     d3.zoomTransform(svg.node()).invert([width / 2, height / 2]),
-    //   );
-    // }
-
     function clicked(event, d) {
       const [[x0, y0], [x1, y1]] = path.bounds(d);
       const stateName = event.target.__data__.properties.name;
       updateClosest(stateName);
       hoist.api.forward(event, "neighbors");
-
       event.stopPropagation();
-      // console.log(stateName);
-      // console.log(s.keys[stateName]);
-
-      /*
-
-
-      states.transition().style("fill", null);
-      d3.select(this).transition().style("fill", "red");
-      svg.transition().duration(750).call(
-        zoom.transform,
-        d3.zoomIdentity
-          .translate(width / 2, height / 2)
-          .scale(
-            Math.min(8, 0.9 / Math.max((x1 - x0) / width, (y1 - y0) / height)),
-          )
-          .translate(-(x0 + x1) / 2, -(y0 + y1) / 2),
-        d3.pointer(event, svg.node()),
-      );
-      */
     }
-
-    // function zoomed(event) {
-    //   const { transform } = event;
-    //   g.attr("transform", transform);
-    //   g.attr("stroke-width", 1 / transform.k);
-    // }
-
     container.append(svg.node());
   }
 }
